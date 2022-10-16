b76f27532d686aab28f5d31cc7557258235078e9
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-17 11:56:34 +1300

    switch case added

--- players.cpp
+++ players.cpp
@@ -6,33 +6,52 @@ int main () {
 
     struct soccer {
     int playerNumber; 
-    int playerNumber2;
     float topSpeed;
+    } player1;
+
+    struct soccer {
+    int playerNumber2;
     float topSpeed2;
-    };
+    } player2;
 
     enum playerPosition { goalkeeper, midfielder, striker, winger, defender};
 
     char position1, position2;
 
-    soccer s;
-
     cout << "Enter player 1 number" << endl;
-    cin >> s.playerNumber;
+    cin >> player1.playerNumber;
     cout << "Enter player 1 top speed in miles per h (only input miles)" << endl;
-    cin >> s.topSpeed;
-    cout << "Enter player 1 position type with first letter of position example: striker = s" << endl;
+    cin >> player1.topSpeed;
+    cout << "Enter player 1 position type with first letter of position example: striker = player2" << endl;
     cin >> position1;
     cout << "\n" << "\n";
     cout << "Enter player 2 number" << endl;
-    cin >> s.playerNumber2;
+    cin >> player2.playerNumber2;
     cout << "Enter player 2 top speed in miles per h (only input miles)" << endl;
-    cin >> s.topSpeed2;
+    cin >> player2.topSpeed2;
     cout << "Enter player 2 position type with first letter of position example: striker = s" << endl;
     cin >> position2;
 
-    cout << "player1 number:" << s.playerNumber << "|  player1 top speed:" << s.topSpeed << "|  player1 position:" << position1 <<  endl;
-    cout << "player2 number:" << s.playerNumber2 << "|  player2 top speed:" << s.topSpeed2 << "|  player1 position:" << position2 << endl;
+    cout << "player1 number:" << player1.playerNumber << "  |  player1 top speed:" << player1.topSpeed;
+    
+    switch(position1) {
+        case 'd':
+            cout << "  |  player1 position:" << defender <<  endl;
+            break;
+        case 's':
+            cout << "  |  player1 position:" << striker <<  endl;
+            break;
+        case 'm':
+            cout << "  |  player1 position:" << midfielder <<  endl;
+            break;
+        case 'w':
+            cout << "  |  player1 position:" << winger <<  endl;
+            break;
+        default:
+            cout << "  |  player1 position:" << goalkeeper <<  endl;
+    }
+
+    cout << "player2 number:" << player2.playerNumber2 << "  |  player2 top speed:" << player2.topSpeed2 << "  |  player1 position:" << position2 << endl;
 
     return 0;
 }
