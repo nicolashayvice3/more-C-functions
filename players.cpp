78870f849b8c9b3cc78b7c8b1d1d7716fc6ad563
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-17 11:07:33 +1300

    player inputs

--- players.cpp
+++ players.cpp
@@ -5,36 +5,34 @@ using namespace std;
 int main () {
 
     struct soccer {
-    int playerNumber;
+    int playerNumber; 
     int playerNumber2;
     float topSpeed;
     float topSpeed2;
-    } ;
-
-    struct soccer p1 = { 0, 0, 0, 0 };
-    struct soccer* p2 = &p1;
-
-    int player1 = 0;
-
-    int player2 = 0;
+    };
 
     enum playerPosition { goalkeeper, midfielder, striker, winger, defender};
 
+    char position1, position2;
+
+    soccer s;
+
     cout << "Enter player 1 number" << endl;
-    cin >> p2->playerNumber;
+    cin >> s.playerNumber;
     cout << "Enter player 1 top speed in miles per h (only input miles)" << endl;
-    cin >> p2->topSpeed;
+    cin >> s.topSpeed;
     cout << "Enter player 1 position type with first letter of position example: striker = s" << endl;
-    cin >> player1;
+    cin >> position1;
     cout << "\n" << "\n";
     cout << "Enter player 2 number" << endl;
-    cin >> p2->playerNumber2;
+    cin >> s.playerNumber2;
     cout << "Enter player 2 top speed in miles per h (only input miles)" << endl;
-    cin >> p2->topSpeed2;
+    cin >> s.topSpeed2;
     cout << "Enter player 2 position type with first letter of position example: striker = s" << endl;
-    cin >> player2;
+    cin >> position2;
 
-    cout << "player1:" << player1 << " player2:" << player2 <<  endl;
+    cout << "player1:" << s.playerNumber << " player1 top speed:" << s.topSpeed << " player1 position:" << position1 <<  endl;
+    cout << "player2:" << s.playerNumber2 << " player2 top speed:" << s.topSpeed2 << " player1 position:" << position2 << endl;
 
     return 0;
 }
