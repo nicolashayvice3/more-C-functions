091e475e51e1edd0e1e09f3f537074a526f48f4a
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-20 12:05:38 +1300

    player function works

--- players.cpp
+++ players.cpp
@@ -7,23 +7,21 @@ int main () {
     struct soccer {
     int playerNumber; 
     float topSpeed;
-    enum playerPosition1 { goalkeeper, midfielder, striker, winger, defender};
     } player1;
 
-    struct soccer {
+    struct soccer2 {
     int playerNumber2;
     float topSpeed2;
-    enum playerPosition2 { goalkeeper, midfielder, striker, winger, defender};
     } player2;
 
-
+    enum playerPosition1 { goalkeeper, midfielder, striker, winger, defender };
     char position1, position2;
 
     cout << "Enter player 1 number" << endl;
     cin >> player1.playerNumber;
     cout << "Enter player 1 top speed in miles per h (only input miles)" << endl;
     cin >> player1.topSpeed;
-    cout << "Enter player 1 position type with first letter of position example: striker = player2" << endl;
+    cout << "Enter player 1 position type with first letter of position example: winger = w" << endl;
     cin >> position1;
     cout << "\n" << "\n";
     cout << "Enter player 2 number" << endl;
@@ -37,22 +35,39 @@ int main () {
     
     switch(position1) {
         case 'd':
-            cout << "  |  player1 position:" << player1.defender <<  endl;
+            cout << "  |  player1 position:" << " defender" <<  endl;
             break;
         case 's':
-            cout << "  |  player1 position:" << player1.striker <<  endl;
+            cout << "  |  player1 position:" << " striker" <<  endl;
             break;
         case 'm':
-            cout << "  |  player1 position:" << player1.midfielder <<  endl;
+            cout << "  |  player1 position:" << " midfielder" <<  endl;
             break;
         case 'w':
-            cout << "  |  player1 position:" << player1.winger <<  endl;
+            cout << "  |  player1 position:" << " winger" <<  endl;
             break;
         default:
-            cout << "  |  player1 position:" << player1.goalkeeper <<  endl;
+            cout << "  |  player1 position:" << " goalkeeper" <<  endl;
     }
 
-    cout << "player2 number:" << player2.playerNumber2 << "  |  player2 top speed:" << player2.topSpeed2 << "  |  player1 position:" << position2 << endl;
+    cout << "player2 number:" << player2.playerNumber2 << "  |  player2 top speed:" << player2.topSpeed2;
+
+    switch(position2) {
+        case 'd':
+            cout << "  |  player2 position:" << " defender" <<  endl;
+            break;
+        case 's':
+            cout << "  |  player2 position:" << " striker" <<  endl;
+            break;
+        case 'm':
+            cout << "  |  player2 position:" << " midfielder" <<  endl;
+            break;
+        case 'w':
+            cout << "  |  player2 position:" << " winger" <<  endl;
+            break;
+        default:
+            cout << "  |  player2 position:" << " goalkeeper" <<  endl;
+    }
 
     return 0;
 }
