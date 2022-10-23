e0d3440a03916f2f8e0b95021a28b17256c7802e
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-24 11:48:57 +1300

    storing vector for all inputs

--- pointer.cpp
+++ pointer.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 
 int main () {
+
     int numberOfInputs = 0;
 
     cout << "how many inputs? " << endl;
     cin >> numberOfInputs;
+
+    vector<int> vectorOfInputs(numberOfInputs);
 
     return 0;
 }
