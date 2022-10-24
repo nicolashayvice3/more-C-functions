459235dde3c8d68870d53188ce0b9fda2417ebe4
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-24 13:48:47 +1300

    a and b matrix done

--- threeXThree.cpp
+++ threeXThree.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main () {
 
+    int first [3][3];
+    int second [3][3];
+    int sum [3][3];
+
+    cout << endl << "|Input Matrix a|" << "\n\n";
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << "Enter the value for a" << i + 1 << j + 1 << ": ";
+            cin >> first[i][j];
+        }
+    }
+
+    cout << endl << "|Input Matrix a|" << "\n\n";
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << "Enter the value for a" << i + 1 << j + 1 << ": ";
+            cin >> first[i][j];
+        }
+    }
+
     return 0;
 }
