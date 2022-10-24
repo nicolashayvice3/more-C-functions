f41b927549b2a5ed574b6c16bed78e251eb687e1
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-24 13:22:49 +1300

    pointer function fully functional

--- pointer.cpp
+++ pointer.cpp
@@ -3,45 +3,44 @@
 
 using namespace std;
 
+
+// passing array to function using a pointer
+
+void func( vector <int> duplicate) {  // the original vector is copied into the vector duplicate
+     int l = duplicate.size(); // storing the size of vector duplicate in l
+     int sum = 0;
+
+    for(int i = 0; i < l; i++){
+        if(duplicate[i] > 0)
+        sum += duplicate[i];
+    }
+
+     // printing all the elements in the array
+     cout<<"all values in the vector(array)-->"<<endl;
+    for (int i = 0; i < l; i++)
+    cout<<duplicate[i]<<" ";
+    cout<<endl;
+    cout<<"the sum of all the above positive numbers is: "<< sum <<endl;
+} 
+
 int main () {
 
     int numberOfInputs = 0;
-
-    cout << "how many inputs do you want? " << endl;
-    cin >> numberOfInputs;
-
-    // contains all the inputs from user, positive and negative
+    cout<<"How many inputs?"<<endl;
+    cin>>numberOfInputs;
+    
+    //populates the array with all the choosen inputs
     vector<int> vectorOfInputs(numberOfInputs);
-
-    // stores all  positive inputs from user
-    int sum = 0;
-    int countPositive = 0;
-
-    // populates the container with user inputs
-     for(int i = 0; i < numberOfInputs; ++i)
+    
+    // use for loop or any other loop populate the vector by user input
+    for(int i = 0; i < numberOfInputs; ++i)
     {
-        cout<<"input#"<<i+1<<":";
+        cout<<"input#"<<i+1<<": ";
         cin >> vectorOfInputs.at(i);
-        //if the number is positive increase sum
-        if(vectorOfInputs.at(i) >0)
-        {
-            sum += vectorOfInputs.at(i);
-            ++countPositive;
-        }
-        
     }
 
-    //print all the positive numbers
-    for(int elem: vectorOfInputs)
-    {
-        if(elem>0)
-        {
-            cout<<elem<<endl;
-        }
-    }
-
-    //print the sum of all positive inputs 
-    cout<<"the sum of all the above positive numbers is: "<<sum<<endl;
+    // passing the dynamic array as a pointer 
+    func(vectorOfInputs);
 
     return 0;
 }
