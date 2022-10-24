bc5c4ce34146f64ceaad050c74586eae4c65241c
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-24 14:02:32 +1300

    matrix addition

--- threeXThree.cpp
+++ threeXThree.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main () {
 
+    // arrays a, b and c
     int first [3][3];
     int second [3][3];
     int sum [3][3];
 
+    // user input for first array
     cout << endl << "|Input Matrix a|" << "\n\n";
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
@@ -16,12 +18,44 @@ int main () {
         }
     }
 
+    // user input for second array
     cout << endl << "|Input Matrix a|" << "\n\n";
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
             cout << "Enter the value for a" << i + 1 << j + 1 << ": ";
-            cin >> first[i][j];
+            cin >> second[i][j];
         }
+    }
+
+    //displaying matrix input for first and second
+    cout << endl << "|Entered Matrix a and b|" << "\n\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << "\t";
+        for (int j = 0; j < 3; ++j) {
+            cout << first[i][j] << " ";
+        }
+        cout << "\t";
+        for (int j = 0; j < 3; ++j) {
+            cout << second[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // adding the two arrays together
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            sum[i][j] = first[i][j] + second[i][j];
+        }
+    }
+
+    // displaying the sum of the two arrays in sum
+    cout << endl << "|Sum of A and B in to C" << "\n\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << "\t";
+        for (int j = 0; j < 3; ++j) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
