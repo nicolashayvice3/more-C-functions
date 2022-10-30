c48251c7f17e54074fce803bf1a138a921a93b42
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-30 17:11:52 +1300

    hangman function finished

---
+++ hangMan/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include "hangman_functions.h"
+using namespace std;
+
+//runs all the functions within main
+
+int main()
+{
+  //starts the game with a greeting 
+  greet();
+
+  // variables for all aspects of game like choosen word(yoobee) and misses(max 3)
+  string codeword = "yoobee";
+  string answer = "______";
+  int misses = 0;
+  //vector with all incorrect guesses
+  vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  //while loop to keep game running until all guesses are used or word is guessed
+  while(answer!=codeword && misses < 3)
+  {
+    display_misses(misses);
+    display_status(incorrect, answer);
+
+    cout<<"\n\nPlease enter your guess: ";
+    cin>>letter;
+
+    //for loop to check if letter is in the word
+    for(int i = 0; i<codeword.length(); i++)
+    {
+      if(letter==codeword[i])
+      {
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+    if(guess)
+    {
+      cout<<"\nCorrect!\n";
+    }
+    else
+    {
+      cout<<"\nIncorrect! Another portion of the person has been drawn.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+    guess = false;
+  }
+  
+  // ends the game
+  end_game(answer, codeword);
+  return 0;
+}
